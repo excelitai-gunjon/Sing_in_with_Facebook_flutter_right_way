dd54db85ee299421338cd0792e472bdd11ecc4cd
“bayazid-excelitai” <bayazid@excelitai.com>
2022-11-26 18:08:03 +0600

    fix- sign in with facebook

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:logindemo/screen/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
